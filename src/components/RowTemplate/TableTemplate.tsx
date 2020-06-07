e4809b9d9275283de1573f7c1a5b8ff670590dd8
Paweł Zarzycki <pawelzarzyckimail@gmail.com>
2020-06-07 23:51:12 +0200

    create one TableTemplate component with full logic of table

---
+++ src/components/RowTemplate/TableTemplate.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Box } from './styled/Box/Box';
+import { Row } from './styled/Row/Row';
+
+function TableTemplate() {
+
+    const data = {
+        hours: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
+        box: ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    };
+
+    const rowTemplate = data.box.map((boxData, index) => (
+        <Box key={index}>{boxData}</Box>
+    ));
+
+    const displayTable = data.hours.map((hour, index) => (
+        <Row>
+            <Box key={index}>{hour}</Box>
+            {rowTemplate}
+        </Row>
+
+    ));
+    return(
+        <>
+        {displayTable}
+        </>
+    )
+};
+export { TableTemplate };
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,29 +1,14 @@
 import React from 'react';
-import { Row } from './styled/Row/Row';
-import { Columns } from './styled/Columns/Columns';
-import { Box } from './styled/Box/Box';
-import { RowTemplate } from '../RowTemplate/RowTemplate';
+import { TableTemplate } from '../RowTemplate/TableTemplate';
 
 
 function Table() {
     
-    const data = {
-        hours: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
-        box: ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-    };
-
-    const displayColumns = data.hours.map((hour, index) => (
-        <Row>
-            <Box key={index}>{hour}</Box>
-            <RowTemplate />
-        </Row>
-
-    ));
+   
 
     return (
         <>
-            {displayColumns}
-
+            <TableTemplate/>
         </>
     )
 };
